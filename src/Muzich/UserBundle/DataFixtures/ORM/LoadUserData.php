4acc07325232415919f420acc2f5cf70cfd538ae
bastien <bastien.sevajol@sporteasy.fr>
2011-09-14 10:44:26 +0200

    Ajout fixtures: Relations (follow) entre Users.

--- src/Muzich/UserBundle/DataFixtures/ORM/LoadUserData.php
+++ src/Muzich/UserBundle/DataFixtures/ORM/LoadUserData.php
@@ -7,6 +7,7 @@ use Doctrine\Common\DataFixtures\OrderedFixtureInterface;
 use Symfony\Component\DependencyInjection\ContainerAwareInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 use Muzich\CoreBundle\Entity\User;
+use Muzich\CoreBundle\Entity\FollowUser;
 
 class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, ContainerAwareInterface
 {
@@ -32,6 +33,8 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, C
    * @param string $email
    * @param string $password_raw
    * @param array $roles
+   * 
+   * @return User
    */
   protected function createUser($username, $email, $password_raw, $superadmin = false, $enabled = true)
   {
@@ -49,6 +52,17 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, C
     $this->user_manager->updateUser($user, false);
     $this->entity_manager->persist($user);
     $this->addReference('user_'.$username, $user);
+    
+    return $user;
+  }
+  
+  protected function heFollowHim($follower, $followed)
+  {
+    $heFollowHim = new FollowUser();
+    $heFollowHim->setFollower($follower);
+    $heFollowHim->setFollowed($followed);
+    $follower->addFollowUser($heFollowHim);
+    $this->entity_manager->persist($heFollowHim);
   }
   
   public function load($entity_manager)
@@ -56,13 +70,19 @@ class LoadUserData extends AbstractFixture implements OrderedFixtureInterface, C
     $this->entity_manager = $entity_manager;
     $this->user_manager = $this->container->get('fos_user.user_manager');
     
-    $this->createUser('admin', 'admin@root', 'toor');
-    $this->createUser('bux', 'bux@root', 'toor');
-    $this->createUser('jean', 'jean@root', 'toor');
-    $this->createUser('paul', 'paul@root', 'toor');
-    $this->createUser('bob', 'bob@root', 'toor');
-    $this->createUser('joelle', 'joelle@root', 'toor');
+    // Création des Users
+    $admin  = $this->createUser('admin', 'admin@root', 'toor');
+    $bux    = $this->createUser('bux', 'bux@root', 'toor');
+    $jean   = $this->createUser('jean', 'jean@root', 'toor');
+    $paul   = $this->createUser('paul', 'paul@root', 'toor');
+    $bob    = $this->createUser('bob', 'bob@root', 'toor');
+    $joelle = $this->createUser('joelle', 'joelle@root', 'toor');
 
+    // Relations
+    $this->heFollowHim($bux, $jean);
+    $this->heFollowHim($bux, $paul);
+    $this->heFollowHim($joelle, $bux);
+    
     $this->entity_manager->flush();
   }
 }
