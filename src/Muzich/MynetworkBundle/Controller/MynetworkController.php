ecfd587779cedca7a1a81ad76c62054881b8578c
bastien <bastien.sevajol@sporteasy.fr>
2011-09-27 14:42:58 +0200

    Création du Bundle Mynetwork.

---
+++ src/Muzich/MynetworkBundle/Controller/MynetworkController.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Muzich\MynetworkBundle\Controller;
+
+use Muzich\CoreBundle\lib\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+
+class MynetworkController extends Controller
+{
+  
+  /**
+   * 
+   * @Template()
+   */
+  public function indexAction()
+  {
+    return array();
+  }
+  
+}
