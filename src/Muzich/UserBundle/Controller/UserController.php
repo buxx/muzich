e95ad62fbca943759dcd3a1d7564486b168e31be
bastien <sevajol.bastien@gmail.com>
2011-09-30 13:22:13 +0200

    Mise en place de la procédure de changement de mot de passe.

--- src/Muzich/UserBundle/Controller/UserController.php
+++ src/Muzich/UserBundle/Controller/UserController.php
@@ -2,18 +2,57 @@
 
 namespace Muzich\UserBundle\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Muzich\CoreBundle\lib\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use FOS\UserBundle\Model\UserInterface;
 
 class UserController extends Controller
 {
-    /**
-     * @Template()
-     */
-    public function accountAction()
-    {
-        return array(
-          
-        );
+  /**
+   * @Template()
+   */
+  public function accountAction()
+  {
+    $user = $this->getUser();
+
+    $form_password = $this->container->get('fos_user.change_password.form');
+
+      return array(
+        'user' => $user,
+        'form_password' => $form_password->createView()
+      );
+  }
+    
+  public function changePasswordAction()
+  {
+    $user = $this->getUser();
+    if (!is_object($user) || !$user instanceof UserInterface) {
+        throw new AccessDeniedException('This user does not have access to this section.');
     }
+
+    $form = $this->container->get('fos_user.change_password.form');
+    $formHandler = $this->container->get('fos_user.change_password.form.handler');
+    
+    $process = $formHandler->process($user);
+    if ($process)
+    {
+      $this->container->get('session')->setFlash('fos_user_success', 'change_password.flash.success');
+      return new RedirectResponse($this->generateUrl('my_account'));
+    }
+    else
+    {
+      return $this->container->get('templating')->renderResponse(
+        'MuzichUserBundle:User:account.html.twig',
+        array(
+          'form_password' => $form->createView(),
+          'user' => $user
+        )
+      );
+    }
+
+    
+  }
+    
 }
