0322593a5ac385932ffa2c6c008e406405612498
bastien <sevajol.bastien@gmail.com>
2011-10-10 17:18:03 +0200

    Mise en place de traduction pour les messges flash.

--- src/Muzich/UserBundle/Controller/ResettingController.php
+++ src/Muzich/UserBundle/Controller/ResettingController.php
@@ -14,40 +14,6 @@ use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
  */
 class ResettingController extends BaseResettingController
 {
-  
-  /**
-   * Reset user password
-   */
-  public function resetAction($token)
-  {
-      $user = $this->container->get('fos_user.user_manager')->findUserByConfirmationToken($token);
-
-      if (null === $user){
-          throw new NotFoundHttpException(sprintf('The user with "confirmation token" does not exist for value "%s"', $token));
-      }
-
-      if (!$user->isPasswordRequestNonExpired($this->container->getParameter('fos_user.resetting.token_ttl'))) {
-          return new RedirectResponse($this->container->get('router')->generate('fos_user_resetting_request'));
-      }
-
-      $form = $this->container->get('fos_user.resetting.form');
-      $formHandler = $this->container->get('fos_user.resetting.form.handler');
-      $process = $formHandler->process($user);
-
-      if ($process) {
-          $this->authenticateUser($user);
-
-          $this->setFlash('success', 'Votre mot de passe a été mis a jour avec succés.');
-
-          return new RedirectResponse($this->getRedirectionUrl($user));
-      }
-
-      return $this->container->get('templating')->renderResponse('FOSUserBundle:Resetting:reset.html.'.$this->getEngine(), array(
-          'token' => $token,
-          'form' => $form->createView(),
-          'theme' => $this->container->getParameter('fos_user.template.theme'),
-      ));
-  }
 
   /**
    * Generate the redirection url when the resetting is completed.
