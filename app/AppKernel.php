25d2d9996dfb3887394444f6bf6ac0a664fd1e02
bastien <sevajol.bastien@gmail.com>
2011-10-12 15:28:02 +0200

    Desactivation bundle SonataUser

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -30,7 +30,6 @@ class AppKernel extends Kernel
             new Muzich\HomeBundle\MuzichHomeBundle(),
             new Muzich\MynetworkBundle\MuzichMynetworkBundle(),
             new Muzich\AdminBundle\MuzichAdminBundle(),
-            new Sonata\UserBundle\SonataUserBundle(),
         );
 
         if (in_array($this->getEnvironment(), array('dev', 'test'))) {
