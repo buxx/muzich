6a7c4fb524d1a0158c9a0c430547b41d28e80156
bastien <sevajol.bastien@gmail.com>
2011-12-26 13:57:13 +0100

    return false dans un js.

--- web/bundles/muzichindex/js/index.js
+++ web/bundles/muzichindex/js/index.js
@@ -3,11 +3,13 @@ $(document).ready(function(){
    $('#registration_link').click(function(){
      $('#registration_box').slideDown("slow");
      $('#login_box').slideUp("slow");
+     return false;
    });
    
    $('#login_link').click(function(){
      $('#login_box').slideDown("slow");
      $('#registration_box').slideUp("slow");
+     return false;
    });
    
    
