cb2e3369d68567996768873924a52ef4b78f3e53
bastien <sevajol.bastien@gmail.com>
2011-12-26 18:04:47 +0100

    Javascript pour afficher/cacher le lecteur embed.

--- web/bundles/muzichhome/js/home.js
+++ web/bundles/muzichhome/js/home.js
@@ -1,16 +1,43 @@
 $(document).ready(function(){
    
+   // Ouverture de la zone "ajouter un element""
    $('#element_add_link').click(function(){
      $('#element_add_box').slideDown("slow");
      $('#element_add_link').hide();
      return false;
    });   
    
+   // Fermeture de la zone "ajouter un element""
    $('#element_add_close_link').click(function(){
      $('#element_add_box').slideUp("slow");
      $('#element_add_link').show();
      return false;
    }); 
    
+//   // Affichage du/des embed si existant
+//   $('li.element').hover(function(){     
+//     $(this).find('div.element_embed').each(function(i){ 
+//       if ($(this).css('display') == 'none')
+//       {
+//         $(this).show();        
+//       }
+//     });
+//   }, function(){});
+
+   // Affichage du/des embed
+   $('a.element_embed_open_link').click(function(){
+     $('a.element_embed_open_link').hide();
+     $('a.element_embed_close_link').show();
+     $(this).parent('li.element').find('div.element_embed').show();
+     return false;
+   });
+   
+   // Fermeture du embed si demandé
+   $('a.element_embed_close_link').click(function(){
+     $('a.element_embed_open_link').show();
+     $('a.element_embed_close_link').hide();
+     $(this).parent('li.element').find('div.element_embed').hide();
+     return false;
+   });
    
  });
