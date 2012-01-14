cf08645aeae487d585bec0143eaccc7e171edbd7
bastien <sevajol.bastien@gmail.com>
2012-01-14 15:11:12 +0100

    Evolution #65: Info en intro + pages statiques d'info

---
+++ src/Muzich/CoreBundle/Controller/InfoController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Muzich\CoreBundle\Controller;
+
+use Muzich\CoreBundle\lib\Controller;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+
+class InfoController extends Controller
+{
+  
+   
+  /**
+   *
+   * @Template() 
+   */
+  public function aboutAction()
+  {
+    return array();
+  }
+  
+  /**
+   *
+   * @Template() 
+   */
+  public function developmentAction()
+  {
+    return array();
+  }
+  
+}
