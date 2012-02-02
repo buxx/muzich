ed7d0c71d9a27731186bd84166f25cb69f65e0f9
bastien <sevajol.bastien@gmail.com>
2012-02-02 15:32:30 +0100

    Afficher ajouter un element: cache le formulaire de filtre

--- web/bundles/muzichhome/js/home.js
+++ web/bundles/muzichhome/js/home.js
@@ -4,6 +4,7 @@ $(document).ready(function(){
    $('#element_add_link').click(function(){
      $('#element_add_box').slideDown("slow");
      $('#element_add_link').hide();
+     $('form[name="search"]').slideUp();
      return false;
    });   
    
@@ -11,6 +12,7 @@ $(document).ready(function(){
    $('#element_add_close_link').click(function(){
      $('#element_add_box').slideUp("slow");
      $('#element_add_link').show();
+     $('form[name="search"]').slideDown();
      return false;
    }); 
    
