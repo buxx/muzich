343d9ddcfad52251cd3c466687cc35c34ebd58a9
bastien <sevajol.bastien@gmail.com>
2012-04-05 17:39:47 +0200

    Evolution #170 I'm a teapot

--- src/Muzich/CoreBundle/Controller/InfoController.php
+++ src/Muzich/CoreBundle/Controller/InfoController.php
@@ -4,6 +4,8 @@ namespace Muzich\CoreBundle\Controller;
 
 use Muzich\CoreBundle\lib\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 
 class InfoController extends Controller
 {
@@ -34,6 +36,11 @@ class InfoController extends Controller
   public function sitesAction()
   {
     return array();
+  }
+  
+  public function teapotAction()
+  {
+    throw new HttpException(418, "I'm a teapot !");
   }
   
 }
