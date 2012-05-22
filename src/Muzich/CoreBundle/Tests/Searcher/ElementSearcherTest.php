9c2134ebf859e093cbbe7536ae6e11da031c7f95
bastien <sevajol.bastien@gmail.com>
2012-05-22 16:37:24 +0200

    Evolution #188: Recherche: tags strict
    Mise a jour du test pour tester les tags stricts.

--- src/Muzich/CoreBundle/Tests/Searcher/ElementSearcherTest.php
+++ src/Muzich/CoreBundle/Tests/Searcher/ElementSearcherTest.php
@@ -335,4 +335,75 @@ class ElementSearcherTest extends UnitTest
     
   }
   
+  public function testTagStrict()
+  {
+    $r = $this->getDoctrine();
+    $bux = $r->getRepository('MuzichCoreBundle:User')
+      ->findOneByUsername('bux')
+    ;
+    $hardtek   = $r->getRepository('MuzichCoreBundle:Tag')->findOneByName('Hardtek');
+    $tribe     = $r->getRepository('MuzichCoreBundle:Tag')->findOneByName('Tribe');
+    $electro   = $r->getRepository('MuzichCoreBundle:Tag')->findOneByName('Electro');
+    $metal     = $r->getRepository('MuzichCoreBundle:Tag')->findOneByName('Metal');
+    $hardcore  = $r->getRepository('MuzichCoreBundle:Tag')->findOneByName('Hardcore');
+    $psytrance = $r->getRepository('MuzichCoreBundle:Tag')->findOneByName('Psytrance');
+    $dubstep   = $r->getRepository('MuzichCoreBundle:Tag')->findOneByName('Dubstep');
+    
+    $es = new ElementSearcher();
+    $es->init(array(
+      'network'   => ElementSearcher::NETWORK_PUBLIC,
+      'tags'      => array(
+        $hardtek->getId() => 'Hardtek', 
+        $tribe->getId()   => 'Tribe'
+      ),
+      'count'      => 5,
+      'tag_strict' => true
+    ));
+    
+    $this->checkElementSearchResults(
+      $es->getElements($r, $bux->getId()), 
+      array(
+        0 => 'All Is Full Of Pain',
+        1 => 'Dj antoine'
+      )
+    );
+    
+    $es = new ElementSearcher();
+    $es->init(array(
+      'network'   => ElementSearcher::NETWORK_PUBLIC,
+      'tags'      => array(
+        $electro->getId()   => 'Electro', 
+        $hardtek->getId()   => 'Hardtek'
+      ),
+      'count'      => 5,
+      'tag_strict' => true
+    ));
+    
+    $this->checkElementSearchResults(
+      $es->getElements($r, $bux->getId()), 
+      array(
+        'KoinkOin - H5N1'
+      )
+    );
+    
+    $es = new ElementSearcher();
+    $es->init(array(
+      'network'   => ElementSearcher::NETWORK_PUBLIC,
+      'tags'      => array(
+        $metal->getId()      => 'Metal', 
+        $hardcore->getId()   => 'Hardcore'
+      ),
+      'count'      => 5,
+      'tag_strict' => true
+    ));
+    
+    $this->checkElementSearchResults(
+      $es->getElements($r, $bux->getId()), 
+      array(
+        'Babylon Pression - Des Tasers et des Pauvres'
+      )
+    );
+    
+  }
+  
 }
