fca09a303600d04326b70f0d1bd0e7c5ae5545c3
Sevajol Bastien <sevajol.bastien@gmail.com>
2013-04-27 15:20:18 +0200

    Anomalie #621: Messages d'erreurs login

--- src/Muzich/IndexBundle/Controller/IndexController.php
+++ src/Muzich/IndexBundle/Controller/IndexController.php
@@ -58,7 +58,7 @@ class IndexController extends Controller
 
     if ($error) {
         // TODO: this is a potential security risk (see http://trac.symfony-project.org/ticket/9523)
-        $error = $error->getMessage();
+        $error = $this->trans('login.fail', array(), 'users');
     }
     // last username entered by the user
     $lastUsername = (null === $session) ? '' : $session->get(SecurityContext::LAST_USERNAME);
