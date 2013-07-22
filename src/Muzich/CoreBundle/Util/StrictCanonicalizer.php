be363775cfc791b47d7866f212ec2f9bd77bf416
Bastien Sevajol <sevajol.bastien@gmail.com>
2013-07-22 17:40:41 +0200

    Ajout d'une canonicalization plus stricte.

--- src/Muzich/CoreBundle/Util/StrictCanonicalizer.php
+++ src/Muzich/CoreBundle/Util/StrictCanonicalizer.php
@@ -46,4 +46,27 @@ class StrictCanonicalizer implements CanonicalizerInterface
     // On retire les accents occidentaux.
     return $this->remove_accent($string);
   }
+  
+  public function canonicalize_stricter($string)
+  {
+    $string = $this->canonicalize($string);
+    
+    // Remove accents from characters
+    $string = iconv('UTF-8', 'ASCII//TRANSLIT', $string);
+
+    // Everything lowercase
+    $string = strtolower($string);
+
+    // Replace all non-word characters by dashes
+    $string = preg_replace("/\W/", "-", $string);
+
+    // Replace double dashes by single dashes
+    $string = preg_replace("/-+/", '-', $string);
+
+    // Trim dashes from the beginning and end of string
+    $string = trim($string, '-');
+
+    return $string;
+  }
+  
 }
